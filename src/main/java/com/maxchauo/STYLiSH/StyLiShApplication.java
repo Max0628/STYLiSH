f220dce3a268afdf7c837c5e460c4075a6d2adbc
Maxchauo0628 <maxchauo0628@gmail.com>
2025-03-10 23:49:41 +0800

    [chore] add git track file.

---
+++ src/main/java/com/maxchauo/STYLiSH/StyLiShApplication.java
@@ -0,0 +1,13 @@
+package com.maxchauo.STYLiSH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StyLiShApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StyLiShApplication.class, args);
+	}
+
+}
