233c232c3c4f5799542982653348b37f7af468cb
Maxchauo0628 <maxchauo0628@gmail.com>
2025-03-10 23:54:37 +0800

    [feat] week 0 part 2 say hi

--- src/main/java/com/maxchauo/STYLiSH/StyLiShApplication.java
+++ src/main/java/com/maxchauo/STYLiSH/StyLiShApplication.java
@@ -2,12 +2,21 @@ package com.maxchauo.STYLiSH;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @SpringBootApplication
+@RestController
 public class StyLiShApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StyLiShApplication.class, args);
 	}
-
+	
+	@GetMapping("/")
+	public String SayHi(){
+		System.out.println("Hi I AM MAX");
+		return "hi i am max!";
+	}
 }
